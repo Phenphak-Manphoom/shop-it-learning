ccf7aafa399881587e3660a511a023bb1bad71af
Tae <phenphak.forlearn@gmail.com>
2024-06-01 17:03:11 +0700

    connecting to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
 import productRoutes from "./routes/ProductRoutes.js";
+import { connectDatabase } from "./config/dbConnect.js";
 const app = express();
 
 dotenv.config({ path: "backend/config/config.env" }); // จะโหลดค่าจากไฟล์ .env ที่อยู่ในโฟลเดอร์ backend/config/ โดยระบุตำแหน่งของไฟล์ .env ที่จะถูกโหลดด้วยพารามิเตอร์ path ในนี้คือ "backend/config/config.env"
+
+//connecting to database
+connectDatabase();
 
 app.use("/api/v1", productRoutes);
 app.listen(process.env.PORT, () => {
