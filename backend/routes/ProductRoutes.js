8d71ca2176838ccc4ca81c7130907f148638d148
Tae <phenphak.forlearn@gmail.com>
2024-06-02 17:51:33 +0700

    feat(product-routes): add route for creating new product via admin

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getProducts } from "../controllers/productControllers.js";
+import { getProducts, newProduct } from "../controllers/productControllers.js";
 
 const productRoutes = express.Router();
 productRoutes.route("/products").get(getProducts);
+productRoutes.route("/admin/products").post(newProduct);
 
 export default productRoutes;
