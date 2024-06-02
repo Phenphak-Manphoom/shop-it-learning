589e918d5fac2df9e244e64ac47d7d21e3722ee3
Tae <phenphak.forlearn@gmail.com>
2024-06-02 17:55:15 +0700

    feat(middleware): add express.json() to parse JSON request bodies

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,15 @@ import dotenv from "dotenv";
 import express from "express";
 import productRoutes from "./routes/ProductRoutes.js";
 import { connectDatabase } from "./config/dbConnect.js";
+
 const app = express();
 
 dotenv.config({ path: "backend/config/config.env" }); // จะโหลดค่าจากไฟล์ .env ที่อยู่ในโฟลเดอร์ backend/config/ โดยระบุตำแหน่งของไฟล์ .env ที่จะถูกโหลดด้วยพารามิเตอร์ path ในนี้คือ "backend/config/config.env"
 
 //connecting to database
 connectDatabase();
-
-app.use("/api/v1", productRoutes);
+app.use(express.json()); //middleware เพื่อช่วยในการแปลงข้อมูล JSON จาก request body
+app.use("/api", productRoutes);
 app.listen(process.env.PORT, () => {
   console.log(
     `Server start on PORT : ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
