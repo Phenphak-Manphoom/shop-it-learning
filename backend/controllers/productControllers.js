4d2054ecc70f2c55780a0fc5ec5e126a8e9eb505
Tae <phenphak.forlearn@gmail.com>
2024-06-05 06:57:43 +0700

    feat(product-controller):added a new endpoint to update the details of a product

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -30,3 +30,22 @@ export const getProductDetails = async (req, res) => {
     product,
   });
 };
+
+// Update product details   =>  /api/products/:id
+export const updateProduct = async (req, res) => {
+  let product = await Product.findById(req?.params?.id);
+
+  if (!product) {
+    return res.status(404).json({
+      error: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json({
+    product,
+  });
+};
