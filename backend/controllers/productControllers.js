247f13927d822cb4304513923e18402a217c9b77
Tae <phenphak.forlearn@gmail.com>
2024-06-04 16:26:35 +0700

    feat(api):add getProductDetails endpoint to retrieve the details of a product by its id

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -16,3 +16,17 @@ export const newProduct = async (req, res) => {
     product,
   });
 };
+
+//Get single product details   =>  /api /products/:id
+export const getProductDetails = async (req, res) => {
+  const product = await Product.findById(req?.params?.id);
+  if (!product) {
+    return res.status(400).json({
+      error: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    product,
+  });
+};
