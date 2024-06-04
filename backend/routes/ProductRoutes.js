576b4ca8bcdda1af34c5dd23579fdd1190ff05e5
Tae <phenphak.forlearn@gmail.com>
2024-06-05 06:59:15 +0700

    feat(routes): add route for updating product details by ID

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -3,11 +3,13 @@ import {
   getProductDetails,
   getProducts,
   newProduct,
+  updateProduct,
 } from "../controllers/productControllers.js";
 
 const productRoutes = express.Router();
 productRoutes.route("/products").get(getProducts);
 productRoutes.route("/admin/products").post(newProduct);
 productRoutes.route("/products/:id").get(getProductDetails);
+productRoutes.route("/products/:id").put(updateProduct);
 
 export default productRoutes;
