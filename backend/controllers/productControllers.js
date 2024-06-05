1448858324e973bbc0a2a90cb3b432a9d86e363a
Tae <phenphak.forlearn@gmail.com>
2024-06-05 07:13:56 +0700

    feat(product-controller:added a new endpoint to delete a product by its ID

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -49,3 +49,20 @@ export const updateProduct = async (req, res) => {
     product,
   });
 };
+
+// delete product details   =>  /api/products/:id
+export const deleteProduct = async (req, res) => {
+  let product = await Product.findById(req?.params?.id);
+
+  if (!product) {
+    return res.status(404).json({
+      error: "Product not found",
+    });
+  }
+
+  await Product.deleteOne();
+
+  res.status(200).json({
+    message: "Product Deleted",
+  });
+};
