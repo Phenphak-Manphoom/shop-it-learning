193b630a6e8e5c4dd44202ceb6aced624deb2f65
Tae <phenphak.forlearn@gmail.com>
2024-06-07 09:20:31 +0700

    docs: add comments to ErrorHandler class for clarity

--- backend/utils/errorHandler.js
+++ backend/utils/errorHandler.js
@@ -1,10 +1,10 @@
 class ErrorHandler extends Error {
   constructor(message, statusCode) {
-    super(message);
+    super(message); //จะเรียกใช้คอนสตรัคเตอร์ของคลาสแม่ (คลาส Error) และส่งผ่านข้อความของข้อผิดพลาด (message) ไปยังคลาส Error
     this.statusCode = statusCode;
 
     //create stack property
-    Error.captureStackTrace(this, this.constructor);
+    Error.captureStackTrace(this, this.constructor); //จะสร้างคุณสมบัติ stack ที่เก็บข้อมูลเกี่ยวกับ stack trace ของข้อผิดพลาดไว้ในอินสแตนซ์ของข้อผิดพลาดที่สร้างขึ้นมา เพื่อให้ง่ายต่อการดีบัก (debugging) และระบุแหล่งที่มาของข้อผิดพลาด
   }
 }
 
