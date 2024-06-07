6cb9594bdaae1ac3c5e8212598dc20bfeabc038d
Tae <phenphak.forlearn@gmail.com>
2024-06-07 09:13:42 +0700

    feat(product-contoller): use ErrorHandler for product not found error

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,4 +1,5 @@
 import Product from "../models/product.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 // get all products   =>  /api/products
 export const getProducts = async (req, res) => {
@@ -18,12 +19,10 @@ export const newProduct = async (req, res) => {
 };
 
 //Get single product details   =>  /api /products/:id
-export const getProductDetails = async (req, res) => {
+export const getProductDetails = async (req, res, next) => {
   const product = await Product.findById(req?.params?.id);
   if (!product) {
-    return res.status(400).json({
-      error: "Product not found",
-    });
+    return next(new ErrorHandler("Product Not Found", 400));
   }
 
   res.status(200).json({
