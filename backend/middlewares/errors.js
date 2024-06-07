01ceb9300ee8a14bd6b78ee9b3a5301dd7c70b57
Tae <phenphak.forlearn@gmail.com>
2024-06-07 18:01:36 +0700

    feat(middleware): add comprehensive error handling middleware to manage errors in both development and production environments

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -3,8 +3,16 @@ export default (err, req, res, next) => {
     statusCode: err?.statusCode || 500,
     message: err?.message || "Internal Server Error",
   };
-
-  res.status(error.statusCode).json({
-    message: error.message,
-  });
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    res.status(error.statusCode).json({
+      message: error.message,
+      error: err,
+      stack: err?.stack,
+    });
+  }
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
 };
