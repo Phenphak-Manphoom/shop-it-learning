020ac9ae655419f05646e7eba59e87fbdd8afc60
Tae <phenphak.forlearn@gmail.com>
2024-06-12 18:41:35 +0700

    feat(product-controller): add pagination capabilities to product endpoint

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -5,10 +5,16 @@ import ErrorHandler from "../utils/errorHandler.js";
 
 // get all products   =>  /api/products
 export const getProducts = catchAsyncError(async (req, res) => {
+  const resPerPage = 4;
   const apiFilters = new APIFilters(Product, req.query).search().filters();
+
   let products = await apiFilters.query;
   let filteredProductCount = products.length;
+
+  apiFilters.pagination(resPerPage);
+  products = await apiFilters.query.clone();
   res.status(200).json({
+    resPerPage,
     filteredProductCount,
     products,
   });
