41396f9e8e22ef10755bfe444977f07d73e205f6
Tae <phenphak.forlearn@gmail.com>
2024-06-13 12:27:02 +0700

    feat(auth-controllers): implement user registration endpoint

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,14 @@
+import catchAsyncError from "../middlewares/catchAsyncError.js";
+import User from "../models/user.js";
+
+export const registerUser = catchAsyncError(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  res.status(201).json({
+    success: true,
+  });
+});
