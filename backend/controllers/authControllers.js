6cc506d9817d162fb9fca3faad8f3ea18316d1b5
Tae <phenphak.forlearn@gmail.com>
2024-06-15 16:51:21 +0700

    feat(auth-controllers): implement user login functionality

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,8 @@
 import catchAsyncError from "../middlewares/catchAsyncError.js";
 import User from "../models/user.js";
+import ErrorHandle from "../utils/errorHandler.js";
 
+//register user =>  /api/register
 export const registerUser = catchAsyncError(async (req, res, next) => {
   const { name, email, password } = req.body;
   const user = await User.create({
@@ -11,6 +13,32 @@ export const registerUser = catchAsyncError(async (req, res, next) => {
 
   const token = user.getJwtToken();
   res.status(201).json({
+    token,
+  });
+});
+
+//login user =>  /api/login
+export const loginUser = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorHandle("Please enter email and password", 400));
+  }
+
+  // find user in the database
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandle("Invalid email or password", 401));
+  }
+
+  //check if password is correct
+  const isPasswordMatch = await user.comparePassword(password);
+  if (!isPasswordMatch) {
+    return next(new ErrorHandle("Invalid email or password", 401));
+  }
+
+  const token = user.getJwtToken();
+
+  res.status(200).json({
     token,
   });
 });
