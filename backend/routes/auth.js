c2cf77f604aed3768f24a7cab6564e192a9b11f9
Tae <phenphak.forlearn@gmail.com>
2024-06-17 17:25:42 +0700

    feat(auth-routes): add user logout route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/authControllers.js";
+import {
+  loginUser,
+  logout,
+  registerUser,
+} from "../controllers/authControllers.js";
 
 const authRoutes = express.Router();
 authRoutes.route("/register").post(registerUser);
 authRoutes.route("/login").post(loginUser);
+authRoutes.route("/logout").get(logout);
 
 export default authRoutes;
