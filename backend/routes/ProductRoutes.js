3521b5be523ea1a8104642685483710768809983
Tae <phenphak.forlearn@gmail.com>
2024-06-18 10:50:34 +0700

    feat(product-routes): add role-based authorization to product route

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -6,13 +6,15 @@ import {
   newProduct,
   updateProduct,
 } from "../controllers/productControllers.js";
-import { isAuthenticatedUser } from "../middlewares/auth.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 
 const productRoutes = express.Router();
-productRoutes.route("/products").get(isAuthenticatedUser, getProducts);
+productRoutes
+  .route("/products")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getProducts);
 productRoutes.route("/admin/products").post(newProduct);
 productRoutes.route("/products/:id").get(getProductDetails);
-productRoutes.route("/products/:id").put(updateProduct);
-productRoutes.route("/products/:id").delete(deleteProduct);
+productRoutes.route("/admin/products/:id").put(updateProduct);
+productRoutes.route("/admin/products/:id").delete(deleteProduct);
 
 export default productRoutes;
