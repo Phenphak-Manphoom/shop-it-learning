46599a41290465b2d811a3dcece8d2859fc024e3
Tae <phenphak.forlearn@gmail.com>
2024-06-21 15:19:50 +0700

    feat(auth-controller): add forgotPassword functionality and email sending capability

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,8 @@
 import catchAsyncError from "../middlewares/catchAsyncError.js";
 import User from "../models/user.js";
+import { getResetPasswordTemplate } from "../utils/emailTemplate.js";
 import ErrorHandle from "../utils/errorHandler.js";
+import sendEmail from "../utils/sendEmail.js";
 import sendToken from "../utils/sendToken.js";
 
 //register user =>  /api/register
@@ -46,4 +48,39 @@ export const logout = catchAsyncError(async (req, res, next) => {
   res.status(200).json({
     message: "Logged out",
   });
+});
+
+//forgot password=>  /api/password/forgot
+export const forgotPassword = catchAsyncError(async (req, res, next) => {
+  // find user in the database
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new ErrorHandle("User not found with this email", 404));
+  }
+
+  //get reset password token
+  const resetToken = user.getResetPasswordToken();
+  await user.save();
+
+  //create reset password url
+  const resetUrl = `${process.env.FRONTEND_URL}/api/password/reset/${resetToken}`;
+  const message = getResetPasswordTemplate(user?.name, resetUrl);
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "ShopIT Password Recovery",
+      message,
+    });
+    res.status(200).json({
+      message: `Email sent to : ${user.email}`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+    return next(new ErrorHandle(error?.message, 500));
+  }
+
+  sendToken(user, 200, res);
 });
