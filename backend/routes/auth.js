5935c31f6223df0dab346de13c207e0319394168
Tae <phenphak.forlearn@gmail.com>
2024-06-21 15:23:25 +0700

    feat(auth-routes): add forgot password route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  forgotPassword,
   loginUser,
   logout,
   registerUser,
@@ -9,5 +10,7 @@ const authRoutes = express.Router();
 authRoutes.route("/register").post(registerUser);
 authRoutes.route("/login").post(loginUser);
 authRoutes.route("/logout").get(logout);
+
+authRoutes.route("/password/forgot").post(forgotPassword);
 
 export default authRoutes;
