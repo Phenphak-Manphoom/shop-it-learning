79fa71a94ffe224adab6ab891a9c824ee7e47cfe
Tae <phenphak.forlearn@gmail.com>
2024-06-22 18:14:14 +0700

    feat(auth-routes):add  getUserProfile route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
   forgotPassword,
+  getUserProfile,
   loginUser,
   logout,
   registerUser,
   resetPassword,
 } from "../controllers/authControllers.js";
+import { isAuthenticatedUser } from "../middlewares/auth.js";
 
 const authRoutes = express.Router();
 authRoutes.route("/register").post(registerUser);
@@ -15,5 +17,6 @@ authRoutes.route("/logout").get(logout);
 authRoutes.route("/password/forgot").post(forgotPassword);
 authRoutes.route("/password/reset/:token").put(resetPassword);
 
+authRoutes.route("/me").get(isAuthenticatedUser, getUserProfile);
 
 export default authRoutes;
