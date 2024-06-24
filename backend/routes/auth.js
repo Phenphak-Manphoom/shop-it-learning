b4e06ba331b293d97624221374bcc0cf111f8205
Tae <phenphak.forlearn@gmail.com>
2024-06-24 18:22:49 +0700

    feat(auth-route):add allusers getalluserdetails updateuser and deleteuser routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,9 @@
 import express from "express";
 import {
+  allUsers,
+  deleteUser,
   forgotPassword,
+  getUserDetails,
   getUserProfile,
   loginUser,
   logout,
@@ -8,8 +11,9 @@ import {
   resetPassword,
   updatePassword,
   updateProfile,
+  updateUser,
 } from "../controllers/authControllers.js";
-import { isAuthenticatedUser } from "../middlewares/auth.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 
 const authRoutes = express.Router();
 authRoutes.route("/register").post(registerUser);
@@ -22,5 +26,14 @@ authRoutes.route("/password/reset/:token").put(resetPassword);
 authRoutes.route("/me").get(isAuthenticatedUser, getUserProfile);
 authRoutes.route("/me/update").put(isAuthenticatedUser, updateProfile);
 authRoutes.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+authRoutes
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allUsers);
+authRoutes
+  .route("/admin/users/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUser)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 export default authRoutes;
