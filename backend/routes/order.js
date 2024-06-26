019885b2a6ff01e114bdab5af9cb3b0c6e0aede6
Tae <phenphak.forlearn@gmail.com>
2024-06-26 17:29:03 +0700

    feat(order-routes):add current user orders and get order details route

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,16 @@
 import express from "express";
 import {} from "../controllers/authControllers.js";
 import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
-import { newOrder } from "../controllers/orderControllers.js";
+import {
+  getOrderDetails,
+  myOrders,
+  newOrder,
+} from "../controllers/orderControllers.js";
 
 const orderRoutes = express.Router();
 
 orderRoutes.route("/orders/new").post(isAuthenticatedUser, newOrder);
+orderRoutes.route("/me/orders").get(isAuthenticatedUser, myOrders);
+orderRoutes.route("/orders/:id").get(isAuthenticatedUser, getOrderDetails);
 
 export default orderRoutes;
