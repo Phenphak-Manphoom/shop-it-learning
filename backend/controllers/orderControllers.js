5bca446ad6564f8ac0a32ea0d3b82e86a3cd57ef
Tae <phenphak.forlearn@gmail.com>
2024-06-26 17:27:44 +0700

    feat(order-controller):add  current user orders and get order details functionality

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -2,6 +2,7 @@ import catchAsyncError from "../middlewares/catchAsyncError.js";
 import User from "../models/user.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import Order from "../models/order.js";
+
 //create new order => /api/orders/new
 export const newOrder = catchAsyncError(async (req, res, next) => {
   const {
@@ -26,6 +27,33 @@ export const newOrder = catchAsyncError(async (req, res, next) => {
     paymentInfo,
     user: req.user._id,
   });
+
+  res.status(200).json({
+    order,
+  });
+});
+
+//get current user orders => /api/me/orders
+export const myOrders = catchAsyncError(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+  if (!orders) {
+    return next(new ErrorHandler("No order found with this ID", 404));
+  }
+
+  res.status(200).json({
+    orders,
+  });
+});
+
+//get order details  => /api/orders/:id
+export const getOrderDetails = catchAsyncError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (!order) {
+    return next(new ErrorHandler("No order found with this ID", 404));
+  }
 
   res.status(200).json({
     order,
