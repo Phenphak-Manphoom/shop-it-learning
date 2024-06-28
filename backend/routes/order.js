5958f93972234269921a9aad928cdcb297a3e5c0
Tae <phenphak.forlearn@gmail.com>
2024-06-28 11:10:19 +0700

    feat(order-routes): add admin order management routes

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,9 +2,12 @@ import express from "express";
 import {} from "../controllers/authControllers.js";
 import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 import {
+  deleteOrder,
+  getAllOrders,
   getOrderDetails,
   myOrders,
   newOrder,
+  updateOrder,
 } from "../controllers/orderControllers.js";
 
 const orderRoutes = express.Router();
@@ -12,5 +15,14 @@ const orderRoutes = express.Router();
 orderRoutes.route("/orders/new").post(isAuthenticatedUser, newOrder);
 orderRoutes.route("/me/orders").get(isAuthenticatedUser, myOrders);
 orderRoutes.route("/orders/:id").get(isAuthenticatedUser, getOrderDetails);
+orderRoutes.route("/orders/:id").get(isAuthenticatedUser, getOrderDetails);
+
+orderRoutes
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+orderRoutes
+  .route("/admin/orders/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 export default orderRoutes;
