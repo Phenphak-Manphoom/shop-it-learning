b057caab8f8e8b084a6ac1e56529b1c7f402f2b9
Tae <phenphak.forlearn@gmail.com>
2024-07-19 17:41:54 +0700

    feat: connect to database on server start

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
+import { connectDatabase } from "./config/dbConnect.js";
 
 const app = express();
 dotenv.config({ path: "backend/config/config.env" });
-
+connectDatabase();
 app.use("/api", productRoutes);
 app.listen(process.env.PORT, () => {
   console.log(
