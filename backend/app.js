91e761a0781afd617baa4355db16305e2deb5f85
Tae <phenphak.forlearn@gmail.com>
2024-07-19 15:40:20 +0700

    chore: update server start message to include NODE_ENV

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 const app = express();
 dotenv.config({ path: "backend/config/config.env" });
+
+console.log("Hello");
 app.listen(process.env.PORT, () => {
-  console.log(`Server start is on PORT : ${process.env.PORT}`);
+  console.log(`Server start is on PORT : ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
