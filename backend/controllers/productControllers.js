666c4b98592c6ff7d1c12c6a8674c2f9d11e5d21
Tae <phenphak.forlearn@gmail.com>
2024-07-20 18:31:31 +0700

    feat: add newProduct endpoint to create a new product

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,16 @@
+import Product from "../models/products.js";
+
+//Get product => /api/products
 export const getProducts = async (req, res) => {
   res.status(200).json({
     message: "All Products",
+  });
+};
+
+//Create new product => /api/admin/products
+export const newProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(200).json({
+    product,
   });
 };
