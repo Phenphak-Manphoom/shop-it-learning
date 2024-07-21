d365bcbe4f933865673cf766cfc736c27c53af8a
Tae <phenphak.forlearn@gmail.com>
2024-07-21 15:03:15 +0700

    feat(product-controller): add deleteProduct function for removing products

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -45,3 +45,18 @@ export const updateProduct = async (req, res) => {
     product,
   });
 };
+
+//Delete product => /api/products/:id
+export const deleteProduct = async (req, res) => {
+  const product = await Product.findById(req?.params?.id);
+  if (!product) {
+    return res.status(404).json({
+      error: "Product not found",
+    });
+  }
+
+  await product.deleteOne();
+  res.status(200).json({
+    message: "Product Deleted",
+  });
+};
