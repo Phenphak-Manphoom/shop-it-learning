279ebb7b83be5a324265af31d0795cf7decdbe97
Tae <phenphak.forlearn@gmail.com>
2024-07-24 10:37:18 +0700

    feat(error-handling): add centralized error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import { connectDatabase } from "./config/dbConnect.js";
-
+import errorMiddleware from "./middlewares/error.js";
 const app = express();
 dotenv.config({ path: "backend/config/config.env" });
 connectDatabase();
 app.use(express.json());
 app.use("/api", productRoutes);
+
+//using middleware
+app.use(errorMiddleware);
+
 app.listen(process.env.PORT, () => {
   console.log(
     `Server start is on PORT : ${process.env.PORT} in ${process.env.NODE_ENV} mode`
