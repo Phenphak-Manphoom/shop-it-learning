16329f8aa8a69410c0aef730445c6577c3c85acc
Tae <phenphak.forlearn@gmail.com>
2024-07-24 10:39:18 +0700

    feat: add ErrorHandler class and integrate error handling

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,5 @@
 import Product from "../models/products.js";
-
+import ErrorHandler from "../utils/errorHandler.js";
 //Get product => /api/products
 export const getProducts = async (req, res) => {
   const product = await Product.find();
@@ -17,12 +17,10 @@ export const newProduct = async (req, res) => {
 };
 
 //Get single product => /api/products/:id
-export const getProductDetails = async (req, res) => {
+export const getProductDetails = async (req, res, next) => {
   const product = await Product.findById(req?.params?.id);
   if (!product) {
-    return res.status(404).json({
-      error: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
   res.status(200).json({
     product,
