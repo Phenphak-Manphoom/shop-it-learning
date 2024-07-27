48b065b9ed444275c332859fb7a95b314693ce83
Tae <phenphak.forlearn@gmail.com>
2024-07-27 15:42:00 +0700

    feat(auth): encrypt user password before saving

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -32,4 +32,11 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+//encrypting password before saving the user
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
 export default mongoose.model("User", userSchema);
