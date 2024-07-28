18c58890fad5c95ed79a0e065d2a722a57bb8185
Tae <phenphak.forlearn@gmail.com>
2024-07-28 18:13:23 +0700

    feat(auth): implement JWT token generation on user registration

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -5,7 +5,8 @@ export const registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
   const user = await User.create({ name, email, password });
 
+  const token = user.getJwtToken();
   res.status(201).json({
-    success: true,
+    token,
   });
 });
