69a2155fd177ae34b8f8fe48127b4bbb41a6603c
Tae <phenphak.forlearn@gmail.com>
2024-07-29 13:14:41 +0700

    feat(auth): implement user login with JWT authentication

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,12 +1,37 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import User from "../models/user.js";
-
+import ErrorHandler from "../utils/errorHandler.js";
+//register user => /api/register
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
   const user = await User.create({ name, email, password });
 
   const token = user.getJwtToken();
   res.status(201).json({
+    token,
+  });
+});
+
+//login user => /api/login
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email and password", 400));
+  }
+
+  // find user in the database
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  //check if password in correct
+  const isPasswordMatch = await user.comparePassword(password);
+  if (!isPasswordMatch) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+  const token = user.getJwtToken();
+  res.status(200).json({
     token,
   });
 });
