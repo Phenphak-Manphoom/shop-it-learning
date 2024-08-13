48dfea934a96c8afd8562165f25d4ea4f9e9f822
Tae <phenphak.forlearn@gmail.com>
2024-08-13 16:32:34 +0700

    feat(auth): implement password reset functionality

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -4,6 +4,7 @@ import { getResetPasswordTemplate } from "../utils/emailTemplates.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import sendEmail from "../utils/sendEmail.js";
 import sendToken from "../utils/sendToken.js";
+import crypto from "crypto";
 
 //register user => /api/register
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -80,4 +81,38 @@ export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
     await user.save();
     return next(new ErrorHandler(error?.message, 500));
   }
+});
+
+//reset password => /api/password/reset/:token
+export const resetPassword = catchAsyncErrors(async (req, res, next) => {
+  //hash the URL token
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      new ErrorHandler(
+        "Password reset token is invalid or has been expired ",
+        400
+      )
+    );
+  }
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Passwords does not match", 400));
+  }
+
+  //set the new password
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+  sendToken(user, 200, res);
 });
