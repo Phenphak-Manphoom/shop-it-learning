1fefe7e8e0059003cce02586755290b7ab9a4442
Tae <phenphak.forlearn@gmail.com>
2024-08-15 16:07:11 +0700

    feat(routes): add routes for admin user management

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import {
+  allUser,
   forgotPassword,
+  getUserDetails,
   getUserProfile,
   loginUser,
   logout,
@@ -9,7 +11,7 @@ import {
   updatePassword,
   updateProfile,
 } from "../controllers/authControllers.js";
-import { isAuthenticatedUser } from "../middlewares/auth.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 
 const router = express.Router();
 router.route("/register").post(registerUser);
@@ -22,5 +24,12 @@ router.route("/password/reset/:token").put(resetPassword);
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
 router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allUser);
+router
+  .route("/admin/users/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails);
 
 export default router;
