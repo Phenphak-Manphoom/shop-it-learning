218382e44401ee2d23dfb431a5dd56262c53720a
Tae <phenphak.forlearn@gmail.com>
2024-08-16 16:59:12 +0700

    feat(order): add user-specific and order details retrieval functionalities

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -1,4 +1,5 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import order from "../models/order.js";
 import Oder from "../models/order.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
@@ -26,6 +27,30 @@ export const newOrder = catchAsyncErrors(async (req, res, next) => {
     paymentInfo,
     user: req.user._id,
   });
+
+  res.status(200).json({
+    order,
+  });
+});
+
+//get current user order => /api/me/orders
+export const myOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Oder.find({ user: req.user._id });
+
+  res.status(200).json({
+    orders,
+  });
+});
+
+//get order details => /api/orders/:id
+export const getOrderDetails = catchAsyncErrors(async (req, res, next) => {
+  const order = await Oder.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (!order) {
+    return next(new ErrorHandler("No order found with is ID", 404));
+  }
 
   res.status(200).json({
     order,
