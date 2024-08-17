f1c7914997ac335d66a24a9f5a34fa228a40c296
Tae <phenphak.forlearn@gmail.com>
2024-08-17 13:44:26 +0700

    feat(order): add admin functionalities for viewing and updating orders

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -1,6 +1,7 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import order from "../models/order.js";
 import Oder from "../models/order.js";
+import Product from "../models/products.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
 //create new order => /api/orders/new
@@ -54,5 +55,44 @@ export const getOrderDetails = catchAsyncErrors(async (req, res, next) => {
 
   res.status(200).json({
     order,
+  });
+});
+
+//get all orders - ADMIN => /api/admin/orders
+export const allOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Oder.find();
+
+  res.status(200).json({
+    orders,
+  });
+});
+
+//Update orders - ADMIN => /api/admin/orders/:id
+export const updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Oder.findById(req.params.id);
+  if (!order) {
+    return next(new ErrorHandler("No order found with is ID", 404));
+  }
+  if (order?.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order", 400));
+  }
+
+  //update product stock
+  order?.orderItems?.forEach(async (item) => {
+    const product = await Product.findById(item?.product?.toString());
+    if (!product) {
+      return next(new ErrorHandler("No product found with is ID", 404));
+    }
+    product.stock = product.stock - item.quantity;
+    await product.save({ validateBeforeSave: false });
+  });
+
+  order.orderStatus = req.body.status;
+  order.deliveredAt = Date.now();
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
   });
 });
