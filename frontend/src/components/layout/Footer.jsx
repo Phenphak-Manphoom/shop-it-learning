bcc968163660fb49c31777b9f576ddf70e84781f
Tae <phenphak.forlearn@gmail.com>
2024-08-24 13:28:28 +0700

    feat: implement footer component with fixed position

---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-zinc-50 text-center fixed bottom-0 left-0 w-full dark:bg-neutral-700 lg:text-left">
+      <div className="bg-black/5 p-4 text-center text-surface dark:text-white">
+        ShopIT - 2024, All Rights Reserved
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
