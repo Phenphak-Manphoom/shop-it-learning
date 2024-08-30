41bd3a29357e4a1abecc333b5cfa3bafe9560d6f
Tae <phenphak.forlearn@gmail.com>
2024-08-30 13:45:41 +0700

    feat: add Toaster for notifications and product details route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
+import {Toaster} from "react-hot-toast"
+import ProductDetails from "./components/product/ProductDetails";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Toaster position="top-center" />
         <Header />
         <div className="flex flex-col m-auto ">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+
           </Routes>
         </div>
 
