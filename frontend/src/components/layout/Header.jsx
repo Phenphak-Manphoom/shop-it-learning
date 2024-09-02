c5034d265404104bf8a2395d3beba0972b5a74ce
Tae <phenphak.forlearn@gmail.com>
2024-09-02 15:46:28 +0700

    refactor: move keyword filtering logic from Home to Search component

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Search from "./Search";
 
 const Header = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -38,36 +39,7 @@ const Header = () => {
           </svg>
         </button>
 
-        <form className="max-w-lg mx-auto relative">
-          <input
-            type="search"
-            id="search-dropdown"
-            className="block w-[32rem] p-2 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-            placeholder="Enter Product Name..."
-            required
-          />
-          <button
-            type="submit"
-            className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-orange-600 rounded-e-lg"
-          >
-            <svg
-              className="w-4 h-4"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-            <span className="sr-only">Search</span>
-          </button>
-        </form>
+        <Search />
 
         <div
           className={`hidden w-full md:block md:w-auto ${
