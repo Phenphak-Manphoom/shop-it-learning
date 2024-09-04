0aace1207c0f448544dbab4def72c26b80932144
Tae <phenphak.forlearn@gmail.com>
2024-09-04 15:20:26 +0700

    feat(home): add min and max price filter parameters to product query and update styles for consistency

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,13 +6,20 @@ import Loader from "./layout/Loader";
 import toast from "react-hot-toast";
 import CustomPagination from "./layout/CustomPagination";
 import { useSearchParams } from "react-router-dom";
+import Filters from "./layout/Filters";
 
 const Home = () => {
   let [searchParams] = useSearchParams();
   const page = searchParams.get("page") || 1;
   const keyword = searchParams.get("keyword") || "";
+  const min = searchParams.get("min");
+  const max = searchParams.get("max");
 
   const params = { page, keyword };
+
+  min !== null && (params.min = min);
+  max !== null && (params.max = max);
+
   const { data, isLoading, error, isError } = useGetProductsQuery(params);
 
   useEffect(() => {
@@ -27,40 +34,39 @@ const Home = () => {
     <>
       <MetaData title={"Buy Best Product Online"} />
       <div
-        className={
-          keyword
-            ? "grid grid-cols-1 lg:grid-cols-3 gap-10 mt-10"
-            : "grid grid-cols-1 lg:grid-cols-2 gap-8 mt-10"
-        }
+        className={`min-h-screen  grid grid-cols-1 lg:grid-cols-${
+          keyword ? 3 : 2
+        }   m-auto max-w-screen-xl`}
       >
         {keyword && (
-          <div className="lg:col-span-1 ">
-            <h2 className="text-xl text-center font-semibold ">Filter</h2>
+          <div className="lg:col-span-1 m-auto w-auto max-w-sm">
+            <div className="text-xl text-left p-5  font-semibold ">
+              <Filters />
+            </div>
           </div>
         )}
 
-        <div
-          className={keyword ? "lg:col-span-2 mr-36" : "lg:col-span-2 m-auto"}
-        >
+        <div className="lg:col-span-2 m-auto">
           <h1 className="pb-10 text-2xl">
             {keyword
               ? `${data?.products?.length} Products found with keyword: ${keyword}`
               : "Latest Products"}
           </h1>
           <div
-            className={
-              keyword ? "grid grid-cols-3 gap-4" : "grid grid-cols-4 gap-6"
-            }
+            className={`grid grid-cols-${keyword ? "3" : "4"} gap-${
+              keyword ? "3" : "4"
+            }`}
           >
             {data?.products?.map((product) => (
               <ProductItem product={product} key={product.id} />
             ))}
           </div>
-
-          <CustomPagination
-            resPerPage={data?.resPerPage}
-            filterProductsCount={data?.filterProductsCount}
-          />
+          <div className="my-12">
+            <CustomPagination
+              resPerPage={data?.resPerPage}
+              filterProductsCount={data?.filterProductsCount}
+            />
+          </div>
         </div>
       </div>
     </>
