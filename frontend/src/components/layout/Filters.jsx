1302399f38b26578d2c063889b979172143aff1a
Tae <phenphak.forlearn@gmail.com>
2024-09-05 14:40:55 +0700

    feat(Filters): Implement category and price filter functionality with query handling

--- frontend/src/components/layout/Filters.jsx
+++ frontend/src/components/layout/Filters.jsx
@@ -1,13 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { getPriceQueryParams } from "../../helpers/helper";
+import { PRODUCT_CATEGORIES } from "../../constants/constants";
 
 const Filters = () => {
   const [min, setMin] = useState(0);
   const [max, setMax] = useState(0);
+
   const navigate = useNavigate();
   let [searchParams] = useSearchParams();
 
+  useEffect(() => {
+    searchParams.has("min") && setMin(searchParams.get("min"));
+    searchParams.has("max") && setMax(searchParams.get("max"));
+  }, []);
+
+  //handle category && ratings filter
+  const handleClick = (checkbox) => {
+    const checkboxes = document.getElementsByName(checkbox.name);
+    checkboxes.forEach((item) => {
+      if (item !== checkbox) item.checked = false;
+    });
+    if (checkbox.checked === false) {
+      //delete filter from query
+      if (searchParams.has(checkbox.name)) {
+        searchParams.delete(checkbox.name);
+        const path = window.location.pathname + "?" + searchParams.toString();
+        navigate(path);
+      }
+    } else {
+      //set new filter value if already there
+      if (searchParams.has(checkbox.name)) {
+        searchParams.set(checkbox.name, checkbox.value);
+      } else {
+        //append new filter
+        searchParams.set(checkbox.name, checkbox.value);
+      }
+      const path = window.location.pathname + "?" + searchParams.toString();
+      navigate(path);
+    }
+  };
+
+  //handle price filter
   const handleButtonClick = (e) => {
     e.preventDefault();
     searchParams = getPriceQueryParams(searchParams, "min", min);
@@ -15,6 +49,12 @@ const Filters = () => {
 
     const path = window.location.pathname + "?" + searchParams.toString();
     navigate(path);
+  };
+
+  const defaultCheckHandler = (checkboxType, checkboxValue) => {
+    const value = searchParams.get(checkboxType);
+    if (checkboxValue === value) return true;
+    return false;
   };
 
   return (
@@ -53,36 +93,25 @@ const Filters = () => {
       <hr className="my-3" />
 
       <h5 className="text-base font-medium mb-3">Category</h5>
-      <div className="mb-2">
-        <input
-          className="form-check-input h-4 w-4 border-gray-300 rounded-sm"
-          type="checkbox"
-          name="category"
-          id="check4"
-          value="Category 1"
-        />
-        <label
-          className="ml-2 text-sm font-medium text-gray-700"
-          htmlFor="check4"
-        >
-          Category 1
-        </label>
-      </div>
-      <div className="mb-2">
-        <input
-          className="form-check-input h-4 w-4 border-gray-300 rounded-sm"
-          type="checkbox"
-          name="category"
-          id="check5"
-          value="Category 2"
-        />
-        <label
-          className="ml-2 text-sm font-medium text-gray-700"
-          htmlFor="check5"
-        >
-          Category 2
-        </label>
-      </div>
+      {PRODUCT_CATEGORIES?.map((category) => (
+        <div className="mb-2">
+          <input
+            className="form-check-input h-4 w-4 border-gray-300 rounded-sm"
+            type="checkbox"
+            name="category"
+            id="check4"
+            value={category}
+            defaultChecked={defaultCheckHandler("category", category)}
+            onClick={(e) => handleClick(e.target)}
+          />
+          <label
+            className="ml-2 text-sm font-medium text-gray-700"
+            htmlFor="check4"
+          >
+            {category}
+          </label>
+        </div>
+      ))}
 
       <hr className="my-3" />
 
