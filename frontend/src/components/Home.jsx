01443e101d8ac2b16561f6923ceaa7566474760a
Tae <phenphak.forlearn@gmail.com>
2024-09-05 14:38:31 +0700

    feat(Home): Add category filter and adjust margins

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -14,11 +14,12 @@ const Home = () => {
   const keyword = searchParams.get("keyword") || "";
   const min = searchParams.get("min");
   const max = searchParams.get("max");
-
+  const category = searchParams.get("category");
   const params = { page, keyword };
 
   min !== null && (params.min = min);
   max !== null && (params.max = max);
+  category !== null && (params.category = category);
 
   const { data, isLoading, error, isError } = useGetProductsQuery(params);
 
@@ -39,15 +40,15 @@ const Home = () => {
         }   m-auto max-w-screen-xl`}
       >
         {keyword && (
-          <div className="lg:col-span-1 m-auto w-auto max-w-sm">
+          <div className="lg:col-span-1 m-auto w-auto max-w-sm my-20">
             <div className="text-xl text-left p-5  font-semibold ">
               <Filters />
             </div>
           </div>
         )}
 
-        <div className="lg:col-span-2 m-auto">
-          <h1 className="pb-10 text-2xl">
+        <div className="lg:col-span-2 ">
+          <h1 className="text-2xl my-5">
             {keyword
               ? `${data?.products?.length} Products found with keyword: ${keyword}`
               : "Latest Products"}
@@ -55,13 +56,13 @@ const Home = () => {
           <div
             className={`grid grid-cols-${keyword ? "3" : "4"} gap-${
               keyword ? "3" : "4"
-            }`}
+            } mt-12`}
           >
             {data?.products?.map((product) => (
               <ProductItem product={product} key={product.id} />
             ))}
           </div>
-          <div className="my-12">
+          <div className="mt-10">
             <CustomPagination
               resPerPage={data?.resPerPage}
               filterProductsCount={data?.filterProductsCount}
