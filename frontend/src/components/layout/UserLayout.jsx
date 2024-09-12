ff7d52fdea0ae00a9a870426df532db3253653af
Tae <phenphak.forlearn@gmail.com>
2024-09-12 16:54:29 +0700

    feat(layout): create UserLayout component with SideMenu and content section

---
+++ frontend/src/components/layout/UserLayout.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import SideMenu from "./SideMenu";
+
+const UserLayout = ({ children }) => {
+  // const menuItems = [
+  //   {
+  //     name: "profile",
+  //     url: "/me/profile",
+  //     icon: "fas fa-user",
+  //   },
+  //   {
+  //     name: "Update Profile",
+  //     url: "/me/update_profile",
+  //     icon: "fas fa-user",
+  //   },
+  //   {
+  //     name: "Upload Avatar",
+  //     url: "/me/upload_avatar",
+  //     icon: "fas fa-user-circle",
+  //   },
+  //   {
+  //     name: "Update Password",
+  //     url: "/me/update_password",
+  //     icon: "fas fa-lock",
+  //   },
+  // ];
+  return (
+    <>
+      <div className="mt-2 mb-4 py-4">
+        <h2 className="text-center font-bold text-3xl">User Setting</h2>
+      </div>
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-evenly text-lg font-medium">
+          <div className="w-full lg:w-1/4 mb-4 lg:mb-0 ">
+            <SideMenu />
+          </div>
+          <div className="w-full lg:w-2/3">{children}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserLayout;
