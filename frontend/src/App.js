0f8694866892a818714a65c86bfa6ac0aa888a46
Tae <phenphak.forlearn@gmail.com>
2024-09-26 18:03:14 +0700

    feat(routes): add protected shipping route with ProtectedRoute wrapper

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import UpdatePassword from "./components/ีuser/UpdatePassword";
 import ForgotPassword from "./components/auth/ForgotPassword";
 import ResetPassword from "./components/auth/ResetPassword";
 import Cart from "./components/cart/Cart";
-
+import Shipping from "./components/cart/Shipping";
 
 function App() {
   return (
@@ -67,6 +67,14 @@ function App() {
             />
 
             <Route path="/cart" element={<Cart />} />
+            <Route
+              path="/shipping"
+              element={
+                <ProtectedRoute>
+                  <Shipping />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
         </div>
 
