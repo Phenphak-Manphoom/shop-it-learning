dbe7852f6ea170acf15b800fb3aa5594024768ef
Tae <phenphak.forlearn@gmail.com>
2024-10-13 13:40:25 +0700

    feat(product): add canUserReview function to check user eligibility for product reviews

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -2,6 +2,7 @@ import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import Product from "../models/products.js";
 import APIFilters from "../utils/apiFilters.js";
 import ErrorHandler from "../utils/errorHandler.js";
+import Order from "../models/order.js";
 
 //Get product => /api/products
 export const getProducts = catchAsyncErrors(async (req, res, next) => {
@@ -31,7 +32,9 @@ export const newProduct = catchAsyncErrors(async (req, res) => {
 
 //Get single product => /api/products/:id
 export const getProductDetails = catchAsyncErrors(async (req, res, next) => {
-  const product = await Product.findById(req?.params?.id);
+  const product = await Product.findById(req?.params?.id).populate(
+    "reviews.user"
+  );
   if (!product) {
     return next(new ErrorHandler("Product not found", 404));
   }
@@ -153,5 +156,20 @@ export const deleteReview = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     product,
+  });
+});
+
+//can user review=> /api/can_review
+export const canUserReview = catchAsyncErrors(async (req, res) => {
+  const orders = await Order.find({
+    user: req.user._id,
+    "orderItems.product": req.query.productId,
+  });
+  if (orders.length === 0) {
+    return res.status(200).json({ canReview: false });
+  }
+
+  res.status(200).json({
+    canReview: true,
   });
 });
