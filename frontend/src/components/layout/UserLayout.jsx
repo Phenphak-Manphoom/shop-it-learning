88159d9d63c45d443bd7ebc2fe84d645405d8035
Tae <phenphak.forlearn@gmail.com>
2024-10-25 16:33:43 +0700

    feat: add menu items for user profile and settings in UserLayout component

--- frontend/src/components/layout/UserLayout.jsx
+++ frontend/src/components/layout/UserLayout.jsx
@@ -2,28 +2,28 @@ import React from "react";
 import SideMenu from "./SideMenu";
 
 const UserLayout = ({ children }) => {
-  // const menuItems = [
-  //   {
-  //     name: "profile",
-  //     url: "/me/profile",
-  //     icon: "fas fa-user",
-  //   },
-  //   {
-  //     name: "Update Profile",
-  //     url: "/me/update_profile",
-  //     icon: "fas fa-user",
-  //   },
-  //   {
-  //     name: "Upload Avatar",
-  //     url: "/me/upload_avatar",
-  //     icon: "fas fa-user-circle",
-  //   },
-  //   {
-  //     name: "Update Password",
-  //     url: "/me/update_password",
-  //     icon: "fas fa-lock",
-  //   },
-  // ];
+  const menuItems = [
+    {
+      name: "profile",
+      url: "/me/profile",
+      icon: "fas fa-user",
+    },
+    {
+      name: "Update Profile",
+      url: "/me/update_profile",
+      icon: "fas fa-user",
+    },
+    {
+      name: "Upload Avatar",
+      url: "/me/upload_avatar",
+      icon: "fas fa-user-circle",
+    },
+    {
+      name: "Update Password",
+      url: "/me/update_password",
+      icon: "fas fa-lock",
+    },
+  ];
   return (
     <>
       <div className="mt-2 mb-4 py-4">
@@ -32,7 +32,7 @@ const UserLayout = ({ children }) => {
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row justify-evenly text-lg font-medium">
           <div className="w-full lg:w-1/4 mb-4 lg:mb-0 ">
-            <SideMenu />
+            <SideMenu menuItems={menuItems} />
           </div>
           <div className="w-full lg:w-2/3">{children}</div>
         </div>
