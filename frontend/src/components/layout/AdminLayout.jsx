72e34fb956af784e5d56bdf840fa712dc0017154
Tae <phenphak.forlearn@gmail.com>
2024-10-25 16:20:48 +0700

    feat: implement AdminLayout component with sidebar menu for admin dashboard

---
+++ frontend/src/components/layout/AdminLayout.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import SideMenu from "./SideMenu";
+
+const AdminLayout = ({ children }) => {
+  const menuItems = [
+    {
+      name: "Dashboard",
+      url: "/admin/dashboard",
+      icon: "fas fa-tachometer-alt",
+    },
+    {
+      name: "New Product",
+      url: "/admin/product/new",
+      icon: "fas fa-plus",
+    },
+    {
+      name: "Products",
+      url: "/admin/products",
+      icon: "fab fa-product-hunt",
+    },
+    {
+      name: "Orders",
+      url: "/admin/orders",
+      icon: "fas fa-receipt",
+    },
+    {
+      name: "Users",
+      url: "/admin/users",
+      icon: "fas fa-user",
+    },
+    {
+      name: "Reviews",
+      url: "/admin/reviews",
+      icon: "fas fa-star",
+    },
+  ];
+  return (
+    <>
+      <div className="mt-2 mb-4 py-4">
+        <h2 className="text-center font-bold text-3xl">Admin Dashboard</h2>
+      </div>
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-evenly text-lg font-medium">
+          <div className="w-full lg:w-1/4 mb-4 lg:mb-0 ">
+            <SideMenu menuItems={menuItems} />
+          </div>
+          <div className="w-full lg:w-2/3">{children}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminLayout;
