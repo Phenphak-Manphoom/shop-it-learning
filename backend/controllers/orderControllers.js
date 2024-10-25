ef794f018aa1622c6fb02fa6441fdfb12de678e4
Tae <phenphak.forlearn@gmail.com>
2024-10-25 15:49:33 +0700

    feat(api): add sales data aggregation by date for admin dashboard

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -1,6 +1,6 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import order from "../models/order.js";
-import Oder from "../models/order.js";
+import Order from "../models/order.js";
 import Product from "../models/products.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
@@ -17,7 +17,7 @@ export const newOrder = catchAsyncErrors(async (req, res, next) => {
     paymentInfo,
   } = req.body;
 
-  const order = await Oder.create({
+  const order = await Order.create({
     orderItems,
     shippingInfo,
     itemsPrice,
@@ -36,7 +36,7 @@ export const newOrder = catchAsyncErrors(async (req, res, next) => {
 
 //get current user order => /api/me/orders
 export const myOrders = catchAsyncErrors(async (req, res, next) => {
-  const orders = await Oder.find({ user: req.user._id });
+  const orders = await Order.find({ user: req.user._id });
 
   res.status(200).json({
     orders,
@@ -45,7 +45,7 @@ export const myOrders = catchAsyncErrors(async (req, res, next) => {
 
 //get order details => /api/orders/:id
 export const getOrderDetails = catchAsyncErrors(async (req, res, next) => {
-  const order = await Oder.findById(req.params.id).populate(
+  const order = await Order.findById(req.params.id).populate(
     "user",
     "name email"
   );
@@ -60,7 +60,7 @@ export const getOrderDetails = catchAsyncErrors(async (req, res, next) => {
 
 //get all orders - ADMIN => /api/admin/orders
 export const allOrders = catchAsyncErrors(async (req, res, next) => {
-  const orders = await Oder.find();
+  const orders = await Order.find();
 
   res.status(200).json({
     orders,
@@ -69,7 +69,7 @@ export const allOrders = catchAsyncErrors(async (req, res, next) => {
 
 //Update orders - ADMIN => /api/admin/orders/:id
 export const updateOrder = catchAsyncErrors(async (req, res, next) => {
-  const order = await Oder.findById(req.params.id);
+  const order = await Order.findById(req.params.id);
   if (!order) {
     return next(new ErrorHandler("No order found with is ID", 404));
   }
@@ -99,7 +99,7 @@ export const updateOrder = catchAsyncErrors(async (req, res, next) => {
 
 //delete order  => /api/admin/orders/:id
 export const deleteOrder = catchAsyncErrors(async (req, res, next) => {
-  const order = await Oder.findById(req.params.id);
+  const order = await Order.findById(req.params.id);
   if (!order) {
     return next(new ErrorHandler("No order found with is ID", 404));
   }
@@ -108,5 +108,90 @@ export const deleteOrder = catchAsyncErrors(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
+  });
+});
+
+async function getSalesData(startDate, endDate) {
+  const salesData = await Order.aggregate([
+    {
+      // Stage 1 - Filter results
+      $match: {
+        createdAt: {
+          $gte: new Date(startDate),
+          $lte: new Date(endDate),
+        },
+      },
+    },
+    {
+      // Stage 2 - Group Data
+      $group: {
+        _id: {
+          date: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+        },
+        totalSales: { $sum: "$totalAmount" },
+        numOrders: { $sum: 1 }, // count the number of orders
+      },
+    },
+  ]);
+
+  // Create a Map to store sales data and num of order by data
+  const salesMap = new Map();
+  let totalSales = 0;
+  let totalNumOrders = 0;
+
+  salesData.forEach((entry) => {
+    const date = entry?._id.date;
+    const sales = entry?.totalSales;
+    const numOrders = entry?.numOrders;
+
+    salesMap.set(date, { sales, numOrders });
+    totalSales += sales;
+    totalNumOrders += numOrders;
+  });
+
+  // Generate an array of dates between start & end Date
+  const datesBetween = getDatesBetween(startDate, endDate);
+
+  // Create final sales data array with 0 for dates without sales
+  const finalSalesData = datesBetween.map((date) => ({
+    date,
+    sales: (salesMap.get(date) || { sales: 0 }).sales,
+    numOrders: (salesMap.get(date) || { numOrders: 0 }).numOrders,
+  }));
+  console.log(finalSalesData);
+
+  return { salesData: finalSalesData, totalSales, totalNumOrders };
+}
+
+function getDatesBetween(startDate, endDate) {
+  const dates = [];
+  let currentDate = new Date(startDate);
+
+  while (currentDate <= new Date(endDate)) {
+    const formattedDate = currentDate.toISOString().split("T")[0];
+    dates.push(formattedDate);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return dates;
+}
+
+// Get Sales Data  =>  /api/admin/get_sales
+export const getSales = catchAsyncErrors(async (req, res, next) => {
+  const startDate = new Date(req.query.startDate);
+  const endDate = new Date(req.query.endDate);
+
+  startDate.setUTCHours(0, 0, 0, 0);
+  endDate.setUTCHours(23, 59, 59, 999);
+
+  const { salesData, totalSales, totalNumOrders } = await getSalesData(
+    startDate,
+    endDate
+  );
+
+  res.status(200).json({
+    totalSales,
+    totalNumOrders,
+    sales: salesData,
   });
 });
